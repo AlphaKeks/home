872acce2a9f5c766da383e9fab4a74235abca26e
AlphaKeks <contact@schnose.eu>
2022-07-22 14:19:40 +0200

    updated polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,6 +2,8 @@
 
 killall polybar
 
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+polybar left 2>&1 | tee -a /tmp/polybar.log & disown
+polybar middle 2>&1 | tee -a /tmp/polybar.log & disown
+polybar right 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched."
