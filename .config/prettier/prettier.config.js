3be704a33888f656f3bf0438c6c7ea93ac21e2e4
AlphaKeks <contact@schnose.eu>
2022-08-27 13:18:07 +0200

    addd linter configs

---
+++ .config/prettier/prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	printWidth: 100,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+	quoteProps: "consistent",
+	jsxSingleQuote: true,
+	trailingComma: "none",
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: "always",
+	endOfLine: "lf"
+};
