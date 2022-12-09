2b8884ab0a8c5f4976f23188ce9e881181fa6f40
AlphaKeks <alphakeks@dawn.sh>
2022-12-09 14:14:18 +0100

    nvim

--- .config/nvim/after/queries/rust/injections.scm
+++ .config/nvim/after/queries/rust/injections.scm
@@ -1,9 +1,19 @@
 ; extends
-(
- (line_comment) @_first 
- (_) @rust
- (line_comment) @_last 
- (#match? @_first "^/// ```$") 
- (#match? @_last "^/// ```$")
- (#offset! @rust 0 4 0 4)
+
+( ;; inject highlighting for code blocks in doc comments
+    (line_comment) @_first 
+    (_) @rust
+    (line_comment) @_last 
+    (#match? @_first "^/// ```$") 
+    (#match? @_last "^/// ```$")
+    (#offset! @rust 0 4 0 4)
+)
+
+( ;; inject highlighting for code blocks in normal comments
+    (line_comment) @_first 
+    (_) @rust
+    (line_comment) @_last 
+    (#match? @_first "^// ```$") 
+    (#match? @_last "^// ```$")
+    (#offset! @rust 0 3 0 4)
 )
