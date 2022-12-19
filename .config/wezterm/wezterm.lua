0938bce191e952033981e245c70d1e781e1333fa
AlphaKeks <alphakeks@dawn.sh>
2022-12-19 02:41:43 +0100

    h

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,13 +3,13 @@ local term = require("wezterm")
 return {
 	default_prog = { "/usr/bin/zsh" }, -- don't run as login shell
 	color_scheme = "Catppuccin Mocha",
-	font = term.font("Fira Code Nerd Font Mono"),
-	font_size = 15.0,
-	line_height = 1.25,
+	font = term.font("Fira Code Nerd Font"),
+	font_size = 13.0,
+	line_height = 1.5,
 	use_fancy_tab_bar = false,
 	hide_tab_bar_if_only_one_tab = true,
 	tab_bar_at_bottom = false,
-	window_background_opacity = 1,
+	window_background_opacity = 0.5,
 	window_padding = {
 		left = 4,
 		right = 4,
