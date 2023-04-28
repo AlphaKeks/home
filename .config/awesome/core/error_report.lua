b6814c88f06cff030d7888fbd9780f0d4721121a
AlphaKeks <alphakeks@dawn.sh>
2023-04-29 01:09:39 +0200

    move error handling to its own module

---
+++ .config/awesome/core/error_report.lua
@@ -0,0 +1,32 @@
+local naughty = require("naughty")
+local errors = awesome.startup_errors
+
+if errors then
+  naughty.notify({
+    preset = naughty.config.presets.critical,
+    title = "Errors occurred during startup.",
+    text = tostring(errors),
+  })
+end
+
+do
+  local in_error = false
+
+  awesome.connect_signal("debug::error", function(err)
+    if in_error then
+      return
+    end
+
+    in_error = true
+
+    naughty.notify({
+      preset = naughty.config.presets.critical,
+      title = "Errors occurred during startup.",
+      text = tostring(err),
+    })
+
+    in_error = false
+  end)
+end
+
+-- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=2:softtabstop=2:textwidth=80
--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,12 +1,13 @@
 -- awesome_mode: api-level=4:screen=on
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
+require("awful.autofocus")
+
+-- Report potential errors
+require("core.error_report")
 
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
-require("awful.autofocus")
 -- Widget and layout library
 local wibox = require("wibox")
 -- Theme handling library
@@ -20,18 +21,6 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
-
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-naughty.connect_signal("request::display_error", function(message, startup)
-    naughty.notification {
-        urgency = "critical",
-        title   = "Oops, an error happened"..(startup and " during startup!" or "!"),
-        message = message
-    }
-end)
--- }}}
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
